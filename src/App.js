3e8c0a82b2392edd0133805d583771d26ca06032
Manoj <manoj@Mac.local>
2022-05-06 17:21:16 +0530

    Completed until getting data from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false
-
 function App() {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => alert("Error occured please check the code"));
+  }, []);
+
   return (
-    <div className="App">
-      <h1> apiiii </h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a cryptocurrency</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input" />
+        </form>
+      </div>
     </div>
   );
 }
